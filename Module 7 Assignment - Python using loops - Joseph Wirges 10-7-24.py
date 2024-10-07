91d9468f100158d33115f723aa71f9c128a537a4
jwirges1 <jwirges1@ivytech.edu>
2024-10-07 18:47:22 -0400

    Add files via upload

---
+++ Module 7 Assignment - Python using loops - Joseph Wirges 10-7-24.py
@@ -0,0 +1,25 @@
+Python 3.12.5 (tags/v3.12.5:ff3bc82, Aug  6 2024, 20:45:27) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+>>> for number in numbers:
+...         if number % 2 !=0:
+...                 print(f"{number} is odd")
+...         else:
+...                 print(f"{number} is even")
+... 
+...                 
+1 is odd
+2 is even
+3 is odd
+4 is even
+5 is odd
+6 is even
+7 is odd
+8 is even
+9 is odd
+10 is even
+11 is odd
+12 is even
+13 is odd
+14 is even
+15 is odd
